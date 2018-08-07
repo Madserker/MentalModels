3c2fd5ba3ab9acdad0046e91e62afab2a3bb8d3a
Sergi Hurtado <madserker@gmail.com>
2018-08-07 16:45:06 +0200

    Test1 onClickListener

--- app/src/main/java/com/example/sergi/mentalmodels/MainActivity.kt
+++ app/src/main/java/com/example/sergi/mentalmodels/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.sergi.mentalmodels
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_test1.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val intent = Intent(this@MainActivity.applicationContext, Test::class.java)
+                startActivity(intent)
+
+            }
+        })
     }
 }
--- app/src/main/java/com/example/sergi/mentalmodels/Test.kt
+++ app/src/main/java/com/example/sergi/mentalmodels/Test.kt
@@ -1,5 +1,6 @@
 package com.example.sergi.mentalmodels
 
+import android.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,10 @@ class Test : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+
+        var fragmentTransaction : FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.container, TestFragment1())
+        //fragmentTransaction.add(R.id.container, TestFragment2())
+        fragmentTransaction.commit()
     }
 }
