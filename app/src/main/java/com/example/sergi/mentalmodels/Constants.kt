5f35c367062ae466babf9756fde90becd2a10dd9
Sergi Hurtado <madserker@gmail.com>
2018-08-08 20:16:26 +0200

    states: a, b, c, d, e and f DONE

--- app/src/main/java/com/example/sergi/mentalmodels/Constants.kt
+++ app/src/main/java/com/example/sergi/mentalmodels/Constants.kt
@@ -15,11 +15,54 @@ class Constants{
                 "Sam comença a plora i Robert es posa a vomitar. \n"+
                 "Què fas?"
 
-        val test1_B_question = "Què fas?"
         val test1_B_answer1 = "Consolar a Sam"
         val test1_B_answer2 = "Ajudar a Robert"
         val test1_B_answer3 = "Investigar el cos de Laura"
         val test1_B_image = "test1_b_image"
+
+
+        val test1_C_text = "(Consoles a Sam)\n" +
+                "     Tu: Tranquila… tot s’arreglarà\n" +
+                "     Sam: No ho entens… és tot culpa meva…\n" +
+                "     Tu: Culpa teva? \n" +
+                "     Sam: És igual, deixa-ho estar.\n"
+
+        val test1_C_answer1 = "Insistir"
+        val test1_C_answer2 = "Anar a veure a Robert"
+        val test1_C_answer3 = "Anar a investigar el cos de Laura"
+        val test1_C_image = "test1_cf_image"
+
+
+        val test1_D_text = "(Ajudes a Robert)\n" +
+                "     Tu: Et trobes millor?\n" +
+                "     Robert: Si.. Tot ha sigut culpa de Dani!\n" +
+                "     Tu: De Dani? Per què?\n" +
+                "     Robert: Nunca li va importar ni Sam ni Laura… El que ha passat avui és culpa seva.\n"
+
+        val test1_D_answer1 = "Parlar amb Dani"
+        val test1_D_answer2 = "Investigar el cos de Laura"
+        val test1_D_answer3 = ""
+        val test1_D_image = "test1_d_image"
+
+
+        val test1_E_text = "(Investigues el cos de Laura i trobes al seu costat la jaqueta de Dani amb la seva pistola.)\n Què fas?\n"
+
+        val test1_E_answer1 = "Expliques tot el que has vist als demés."
+        val test1_E_answer2 = "Parles amb Dani sobre el que has vist"
+        val test1_E_answer3 = ""
+        val test1_E_image = "test1_e_image"
+
+        val test1_F_text = "(Insisteixes a Sam)\n" +
+                "     Sam: Estava massa borratxa, en veritat no volia fer res amb Dani..\n" +
+                "     Tu: Tú?... Amb Dani? Junts?\n" +
+                "     Sam: Em vaig equivocar i ara ha passat tot això..\n" +
+                "     Tu: Llavors saps qui ho va fer?\n" +
+                "     Sam: Això no importa ara… És tot culpa meva!\n"
+
+        val test1_F_answer1 = "Insistir més"
+        val test1_F_answer2 = "Ir a veure a Dani"
+        val test1_F_answer3 = "Parlar amb Robert"
+        val test1_F_image = "test1_cf_image"
 
     }
 
--- app/src/main/java/com/example/sergi/mentalmodels/StateMachineTest1.kt
+++ app/src/main/java/com/example/sergi/mentalmodels/StateMachineTest1.kt
@@ -12,33 +12,80 @@ class StateMachineTest1() : StateMachineTest{
 
     override fun option1(){
         if(state=="A"){
-            state = "B"
-            image = Constants.test1_B_image
-            text  = Constants.test1_B_text
-            answer1 = Constants.test1_B_answer1
-            answer2 = Constants.test1_B_answer2
-            answer3 = Constants.test1_B_answer3
+            changeToB()
         }
         else if(state=="B"){
-
-
+            changeToC()
+        }
+        else if(state=="C"){
+            changeToF()
         }
     }
     override fun option2(){
         if(state=="A"){
-
         }
-        else if(state=="B"){
-
+        else if(state=="B" || state=="C"){
+            changeToD()
+        }
+        else if(state=="D"){
+            changeToE()
         }
     }
     override fun option3(){
         if(state=="A"){
-
         }
-        else if(state=="B"){
-
+        else if(state=="B"||state=="C"){
+            changeToE()
         }
+    }
+
+    fun changeToA(){
+        state = "A"
+        image = Constants.test1_A_image
+        text  = Constants.test1_A_text
+        answer1 = Constants.test1_A_answer1
+        answer2 = Constants.test1_A_answer2
+        answer3 = Constants.test1_A_answer3
+    }
+    fun changeToB(){
+        state = "B"
+        image = Constants.test1_B_image
+        text  = Constants.test1_B_text
+        answer1 = Constants.test1_B_answer1
+        answer2 = Constants.test1_B_answer2
+        answer3 = Constants.test1_B_answer3
+    }
+    fun changeToC(){
+        state = "C"
+        image = Constants.test1_C_image
+        text  = Constants.test1_C_text
+        answer1 = Constants.test1_C_answer1
+        answer2 = Constants.test1_C_answer2
+        answer3 = Constants.test1_C_answer3
+    }
+    fun changeToD(){
+        state = "D"
+        image = Constants.test1_D_image
+        text  = Constants.test1_D_text
+        answer1 = Constants.test1_D_answer1
+        answer2 = Constants.test1_D_answer2
+        answer3 = Constants.test1_D_answer3
+    }
+    fun changeToE(){
+        state = "E"
+        image = Constants.test1_E_image
+        text  = Constants.test1_E_text
+        answer1 = Constants.test1_E_answer1
+        answer2 = Constants.test1_E_answer2
+        answer3 = Constants.test1_E_answer3
+    }
+    fun changeToF(){
+        state = "F"
+        image = Constants.test1_F_image
+        text  = Constants.test1_F_text
+        answer1 = Constants.test1_F_answer1
+        answer2 = Constants.test1_F_answer2
+        answer3 = Constants.test1_F_answer3
     }
 
 
