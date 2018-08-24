b0959b15d6d48385c6182991ce6119f15e67bc10
Sergi Hurtado <madserker@gmail.com>
2018-08-24 16:22:44 +0200

    test1 finished

--- app/src/main/java/com/example/sergi/mentalmodels/StateMachineTest1.kt
+++ app/src/main/java/com/example/sergi/mentalmodels/StateMachineTest1.kt
@@ -20,9 +20,31 @@ class StateMachineTest1() : StateMachineTest{
         else if(state=="C"){
             changeToF()
         }
+        else if(state=="D"){
+            changeToG()
+        }
+        else if(state=="E"){
+            changeToH()
+        }
+        else if(state=="F" || state=="G") {
+            changeToI()
+        }
+        else if(state=="H"){
+            changeToK()
+        }
+        else if(state=="I"){
+            changeToJ()
+        }
+        else if(state=="J"){
+            changeToL()
+        }
+        else if(state=="L"){
+            changeToN()
+        }
     }
     override fun option2(){
         if(state=="A"){
+            changeToB()
         }
         else if(state=="B" || state=="C"){
             changeToD()
@@ -30,12 +52,31 @@ class StateMachineTest1() : StateMachineTest{
         else if(state=="D"){
             changeToE()
         }
+        else if(state=="E"||state=="F"){
+            changeToG()
+        }
+        else if(state=="H"){
+            changeToJ()
+        }
+        else if(state=="I" || state=="G"){
+            changeToK()
+        }
+        else if(state=="J"){
+            changeToM()
+        }
+        else if(state=="L"){
+            changeToO()
+        }
     }
     override fun option3(){
-        if(state=="A"){
-        }
-        else if(state=="B"||state=="C"){
+        if(state=="B"||state=="C"){
             changeToE()
+        }
+        else if(state=="F"){
+            changeToD()
+        }
+        else if(state=="H"){
+            changeToI()
         }
     }
 
@@ -87,6 +128,87 @@ class StateMachineTest1() : StateMachineTest{
         answer2 = Constants.test1_F_answer2
         answer3 = Constants.test1_F_answer3
     }
+    fun changeToG(){
+        state = "G"
+        image = Constants.test1_G_image
+        text  = Constants.test1_G_text
+        answer1 = Constants.test1_G_answer1
+        answer2 = Constants.test1_G_answer2
+        answer3 = Constants.test1_G_answer3
+    }
+    fun changeToH(){
+        state = "H"
+        image = Constants.test1_H_image
+        text  = Constants.test1_H_text
+        answer1 = Constants.test1_H_answer1
+        answer2 = Constants.test1_H_answer2
+        answer3 = Constants.test1_H_answer3
+    }
+    fun changeToI(){
+        state = "I"
+        image = Constants.test1_I_image
+        text  = Constants.test1_I_text
+        answer1 = Constants.test1_I_answer1
+        answer2 = Constants.test1_I_answer2
+        answer3 = Constants.test1_I_answer3
+    }
+    fun changeToJ(){
+        state = "J"
+        image = Constants.test1_J_image
+        text  = Constants.test1_J_text
+        answer1 = Constants.test1_J_answer1
+        answer2 = Constants.test1_J_answer2
+        answer3 = Constants.test1_J_answer3
+    }
+    fun changeToK(){
+        state = "K"
+        image = Constants.test1_K_image
+        text  = Constants.test1_K_text
+        answer1 = Constants.test1_K_answer1
+        answer2 = Constants.test1_K_answer2
+        answer3 = Constants.test1_K_answer3
+    }
+    fun changeToL(){
+        state = "L"
+        image = Constants.test1_L_image
+        text  = Constants.test1_L_text
+        answer1 = Constants.test1_L_answer1
+        answer2 = Constants.test1_L_answer2
+        answer3 = Constants.test1_L_answer3
+    }
+    fun changeToM(){
+        state = "M"
+        image = Constants.test1_M_image
+        text  = Constants.test1_M_text
+        answer1 = Constants.test1_M_answer1
+        answer2 = Constants.test1_M_answer2
+        answer3 = Constants.test1_M_answer3
+    }
+    fun changeToN(){
+        state = "N"
+        image = Constants.test1_N_image
+        text  = Constants.test1_N_text
+        answer1 = Constants.test1_N_answer1
+        answer2 = Constants.test1_N_answer2
+        answer3 = Constants.test1_N_answer3
+    }
+    fun changeToO(){
+        state = "O"
+        image = Constants.test1_O_image
+        text  = Constants.test1_O_text
+        answer1 = Constants.test1_O_answer1
+        answer2 = Constants.test1_O_answer2
+        answer3 = Constants.test1_O_answer3
+    }
+
+
+
+
+
+
+
+
+
 
 
 }
